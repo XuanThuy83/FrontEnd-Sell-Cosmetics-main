c9ac048aab5039580ff3fdc1f4d905230d394138
HoangKimSang <hoangkimsang2112@gmail.com>
2025-10-01 09:49:39 +0700

    Cập nhật toàn bộ code

---
+++ assets/js/product.js
@@ -0,0 +1,89 @@
+window.PRODUCTS = [
+  {
+    id: "006",
+    name: "Tinh Chế Loại 1",
+    priceMin: "1.900.000 đ",
+    priceMax: "4.500.000 đ",
+    sold: 390,
+    bullets: [
+      { k: "Chi tiết", v: "Set quà có nhiều sự lựa chọn" },
+      { k: "Hạn sử dụng", v: "12 tháng" },
+      { k: "Hướng dẫn sử dụng", v: "Bật nắp sử dụng ngay. Mỗi ngày 1 hũ. Ngon hơn khi uống lạnh." },
+      { k: "Hướng dẫn bảo quản", v: "Nơi khô thoáng, tránh nắng trực tiếp hoặc ngăn mát tủ lạnh." }
+    ],
+    variants: ["Chọn một tuỳ chọn", "Set cơ bản", "Set cao cấp", "Set quà tặng"],
+    images: [
+      "./assets/img/banyen/tinhche1.jpg",
+      "./assets/img/banyen/tinhche2.jpg",
+      "./assets/img/banyen/tc3.jpg",
+      "./assets/img/banyen/rutlongkho.jpg"
+    ]
+  },
+  {
+    id: "003",
+    name: "Rút Lông Khô",
+    priceMin: "3.900.000 đ",
+    priceMax: "4.500.000 đ",
+    sold: 215,
+    bullets: [
+      { k: "Chi tiết", v: "Đã làm sạch, sấy khô" },
+      { k: "Hạn sử dụng", v: "12 tháng" },
+      { k: "Hướng dẫn sử dụng", v: "Ngâm mềm trước khi nấu" }
+    ],
+    variants: ["Chọn một tuỳ chọn", "Hộp 50gr", "Hộp 100gr"],
+    images: [
+      "./assets/img/banyen/rutlongkho.jpg",
+      "./assets/img/banyen/rutlonguot.jpg"
+    ]
+  },
+  {
+    id: "004",
+    name: "Rút Lông Ướt",
+    priceMin: "3.100.000 đ",
+    priceMax: "3.500.000 đ",
+    sold: 128,
+    bullets: [
+      { k: "Chi tiết", v: "Nhặt sạch, còn ẩm" },
+      { k: "Hạn sử dụng", v: "7 ngày (bảo quản lạnh)" },
+      { k: "Hướng dẫn sử dụng", v: "Dùng ngay hoặc chưng" }
+    ],
+    variants: ["Chọn một tuỳ chọn", "Hũ 50ml x 6", "Hũ 70ml x 6"],
+    images: [
+      "./assets/img/banyen/rutlonguot.jpg",
+      "./assets/img/banyen/soiep.jpg"
+    ]
+  },
+  {
+    id: "012",
+    name: "Yến Thô",
+    priceMin: "2.399.000 đ",
+    priceMax: "2.600.000 đ",
+    sold: 90,
+    bullets: [
+      { k: "Chi tiết", v: "Nguyên tổ, còn lông" },
+      { k: "Hạn sử dụng", v: "12 tháng" },
+      { k: "Hướng dẫn sử dụng", v: "Tự sơ chế trước khi dùng" }
+    ],
+    variants: ["Chọn một tuỳ chọn", "50gr", "100gr"],
+    images: [
+      "./assets/img/banyen/yentho.jpg"
+    ]
+  },
+  {
+    id: "011",
+    name: "Chân Yến Rút Lông",
+    priceMin: "2.800.000 đ",
+    priceMax: "3.000.000 đ",
+    sold: 55,
+    bullets: [
+      { k: "Chi tiết", v: "Phần chân tổ đã làm sạch" },
+      { k: "Hạn sử dụng", v: "12 tháng" },
+      { k: "Hướng dẫn sử dụng", v: "Chưng hoặc nấu súp" }
+    ],
+    variants: ["Chọn một tuỳ chọn", "50gr", "100gr"],
+    images: [
+      "./assets/img/banyen/chanyenrutlong.jpg"
+    ]
+  }
+];
+
